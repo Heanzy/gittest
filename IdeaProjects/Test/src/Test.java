537fa885d0a6f037835e1348c0a2f747ab8666e4
heyanzhao <heyanzhao@360buyad.local>
2018-03-22 09:42:27 +0800

    git test

---
+++ IdeaProjects/Test/src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by heyanzhao on 2018/3/21.
+ */
+import java.util.*;
+public class Test {
+    public  static void main(String [] args){
+        System.out.println("jing dong");
+        System.out.println();/////aaaaaaa
+    }
+
+}
